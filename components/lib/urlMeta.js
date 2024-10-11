c436bb4430b79e385e9cec5fcc9c15fffa882cef
yunkhngn <rogkhoa.mail@gmail.com>
2024-10-11 15:34:58 +0700

    Edit URL

--- components/lib/urlMeta.js
+++ components/lib/urlMeta.js
@@ -2,19 +2,19 @@ const urlMeta = {
     home: {
         route: '/',
         title: 'Trang chủ | Quiz',
-        url: 'https://quiz.hoanguyen.codes/',
+        url: 'https://quiz.khoanguyen.codes/',
         description: 'Quizlet clone by @yunkhngn có các công cụ học để giúp bạn tìm hiểu bất cứ nội dung gì. Cải thiện điểm số và đạt mục tiêu của bạn với thẻ ghi nhớ, bài kiểm tra thử và các lời giải được chuyên gia biên soạn, ngay hôm nay.',
     },
     learning: {
         route: '/learning',
         title: 'Học tập | Quiz',
-        url: 'https://quiz.hoanguyen.codes/learning',
+        url: 'https://quiz.khoanguyen.codes/learning',
         description: 'Chế độ học tập của Quizlet clone.',
     },
     test: {
         route: '/test',
         title: 'Kiểm tra | Quiz',
-        url: 'https://quiz.hoanguyen.codes/test',
+        url: 'https://quiz.khoanguyen.codes/test',
         description: 'Chế độ kiểm tra của Quizlet clone.',
     }
 }
