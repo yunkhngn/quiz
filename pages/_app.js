a718d579945ca8559e93625af6dc7b9ec034355e
yunkhngn <rogkhoa.mail@gmail.com>
2024-10-11 14:37:17 +0700

    Installing atomize

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { styletron } from "../styletron";
+import { Provider as StyletronProvider } from "styletron-react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <StyletronProvider value={styletron}>
+      <Component {...pageProps} />
+    </StyletronProvider>
+  );
 }
